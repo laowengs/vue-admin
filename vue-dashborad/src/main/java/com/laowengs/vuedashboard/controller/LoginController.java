9ab4217c8ba7da0d06c194a5dd0aec901a06fe34
laowengs <369196439@qq.com>
2020-12-31 16:39:33 +0800

    HystrixCommand

--- vue-dashborad/src/main/java/com/laowengs/vuedashboard/controller/LoginController.java
+++ vue-dashborad/src/main/java/com/laowengs/vuedashboard/controller/LoginController.java
@@ -1,6 +1,7 @@
 package com.laowengs.vuedashboard.controller;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -24,8 +25,12 @@ public class LoginController {
         return  result;
     }
 
+    //zuul超时设置1000ms，这里设置为500ms，证明可以重置zuul的超时时间，可以根据不同的接口需求指定不同的超时时间
+    //HystrixCommand的优先级会大于Zuul FallbackProvider接口实现的优先级
     @RequestMapping(value = "sleep/{source}",method = RequestMethod.GET)
-    @HystrixCommand(fallbackMethod = "sleepFallback")
+    @HystrixCommand(fallbackMethod = "sleepFallback", commandProperties = {
+                    @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "500")
+            })
     public String sleep(@PathVariable Integer source) throws InterruptedException {
         Thread.sleep(source.longValue());
         return "sleep:"+source;
